1da3934aaadadfc439646d1d45d70904ae2e0914
Aashifa Parveen <106084971+Aashifaabdul@users.noreply.github.com>
2024-06-24 14:35:20 +0530

    Update and rename breastcancer.ipynb to breastcancer.py

--- breastcancer.py
+++ breastcancer.py
@@ -5,24 +5,7 @@ cancer = datasets.load_breast_cancer()
 print(cancer.feature_names)
 print(cancer.target_names)
 
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, test_size=0.2, random_state=1)
-print(X_train.shape, X_test.shape)
-
-from sklearn.linear_model import LogisticRegression
-# Train the model
-model = LogisticRegression(max_iter=200)
-model.fit(X_train, y_train)
-
-# Make predictions
-y_pred = model.predict(X_test)
-print(y_pred)
-
-from sklearn.metrics import accuracy_score
-accuracy=accuracy_score(y_test,y_pred)
-accuracy
-
-OUTPUT:
+output:
 ['mean radius' 'mean texture' 'mean perimeter' 'mean area'
  'mean smoothness' 'mean compactness' 'mean concavity'
  'mean concave points' 'mean symmetry' 'mean fractal dimension'
@@ -35,9 +18,31 @@ OUTPUT:
 
 ['malignant' 'benign']
 
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, test_size=0.2, random_state=1)
+print(X_train.shape, X_test.shape)
+
+output:
 (455, 30) (114, 30)
+
+from sklearn.linear_model import LogisticRegression
+# Train the model
+model = LogisticRegression(max_iter=200)
+model.fit(X_train, y_train)
+
+# Make predictions
+y_pred = model.predict(X_test)
+print(y_pred)
+
+output:
 [1 0 1 0 0 0 0 0 1 1 1 0 0 1 1 1 1 1 1 0 1 1 0 1 0 1 1 0 0 0 0 1 0 0 1 1 0
  1 1 1 1 1 1 1 1 0 1 1 1 0 0 0 1 1 1 1 1 0 1 1 1 0 1 1 1 1 1 0 1 1 1 1 1 0
  1 0 1 1 1 0 1 0 1 0 1 1 0 1 0 1 1 0 1 1 0 0 1 1 1 1 1 1 1 1 1 1 1 1 0 0 1
  1 1 1]
+
+from sklearn.metrics import accuracy_score
+accuracy=accuracy_score(y_test,y_pred)
+accuracy
+
+output:
 0.956140350877193
