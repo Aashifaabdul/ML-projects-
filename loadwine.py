1f3dc8b9efaa2f496443bf9f7b0438c165aa6ddd
Aashifa Parveen <106084971+Aashifaabdul@users.noreply.github.com>
2024-06-24 14:32:33 +0530

    Update loadwine.py

--- loadwine.py
+++ loadwine.py
@@ -17,3 +17,17 @@ model.fit(X_train, y_train)
 # Make predictions
 y_pred = model.predict(X_test)
 print(y_pred)
+
+
+OUTPUT:
+['alcohol', 'malic_acid', 'ash', 'alcalinity_of_ash', 'magnesium', 'total_phenols', 'flavanoids', 'nonflavanoid_phenols', 'proanthocyanins', 'color_intensity', 'hue', 'od280/od315_of_diluted_wines', 'proline']
+['class_0' 'class_1' 'class_2']
+(142, 13) (36, 13)
+[2 1 0 1 0 2 1 0 2 1 0 1 1 0 1 1 2 0 1 0 0 1 1 1 0 2 0 0 0 2 1 2 2 0 1 1]
+
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(y_test, y_pred)
+accuracy
+
+OUTPUT:
+0.9444444444444444
